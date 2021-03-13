9f03afc9c40f08fa5a7375d90f145d3b688a9e8b
Jyrki Riestola <jyrki.riestola@gmail.com>
2021-03-13 17:34:15 +0200

    välicommit

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const SearchBar = () => {
-  return <div>Hakukenttä</div>;
+  const [searchItem, setSearchItem] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {}, [searchItem]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await fetch("http://localhost:3001/searchitems", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ searchItem }),
+    })
+      .then((res) => res.json())
+      .then((data) => setSearchResults(data));
+  };
+
+  const renderedSearchResults = searchResults.map((item) => {
+    return <li key={item.id}>{item.name.fi}</li>;
+  });
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={searchItem}
+          onChange={(e) => setSearchItem(e.target.value)}
+        />
+        <button type="submit">Hae hakusanalla</button>
+      </form>
+      <ul>{renderedSearchResults}</ul>
+    </div>
+  );
 };
 
 export default SearchBar;
